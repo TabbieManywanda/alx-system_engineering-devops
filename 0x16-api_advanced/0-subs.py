6a7130704d7395d73d6d14c43444f63c163886cd
TabbieManywanda <achiengtabitha@gmail.com>
2023-01-31 06:25:07 -0800

    Pycodestyle edit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -15,7 +15,7 @@ def number_of_subscribers(subreddit):
             "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
             }
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-     response = requests.get(url, headers=headers, allow_redirects=False)
-     if response.status_code == 200:
-         return (response.json().get("data").get("subscribers"))
-     return 0
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        return (response.json().get("data").get("subscribers"))
+    return 0
